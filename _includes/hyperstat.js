e8bcfb90ee94983a674732a7fd6d1adcf349ccf8
Wentong <asyz13jinage@163.com>
2017-03-18 17:35:06 +0800

    Add files via upload

---
+++ _includes/hyperstat.js
@@ -0,0 +1,157 @@
+xlink = "<a href='http://xlstat.com'><img src='xlstat.gif' width='150' height='100' border='0'><br />30 day free trial</a>"
+
+link2 = "<div align = 'left'><a href='http://www.amazon.com/exec/obidos/ASIN/0195086074/hyperstatonline'><img src='biostat.jpg' width='58' height='90' align='left'>An excellent and easy-to-understand introduction to biostatistics. Amazon Review: &quot;Where this book is especially valuable is in keeping things simple -- without the intimidating mathematical notation.&quot;</a></div>"
+
+//link3 = "<a href='http://www.amazon.com/exec/obidos/ASIN/193144210X/hyperstatonline'><b>Printed version of HyperStat <br />is available from Amazon.</b></a>"
+
+link6 = "<iframe src=\"http://rcm.amazon.com/e/cm?t=hyperstatonline&o=1&p=8&l=as1&asins=B000A3WS84&=1&fc1=000000&IS2=1&lt1=_blank&lc1=0000ff&bc1=000000&bg1=ffffff&f=ifr\" style=\"width:120px;height:240px;\" scrolling=\"no\" marginwidth=\"0\" marginheight=\"0\" frameborder=\"0\"></iframe>"
+
+
+//link4 = "<a href='http://www.amazon.com/exec/obidos/ASIN/0071350055/hyperstatonline'><img src='confused2.jpg' width='122' height='157' border='0'></a>"
+
+link4="<iframe src=\"http://rcm.amazon.com/e/cm?t=hyperstatonline&o=1&p=8&l=as1&asins=B00064O1P0&=1&fc1=000000&IS2=1&lt1=_blank&lc1=0000ff&bc1=000000&bg1=ffffff&f=ifr\" style=\"width:120px;height:240px;\" scrolling=\"no\" marginwidth=\"0\" marginheight=\"0\" frameborder=\"0\"></iframe>"
+
+link5 = "<a href='http://talkstats.com'>Talk Stats Forums</a><br />A discussion forum for statistics enthusiasts. Get free statistics help or just hang out and talk about statistics."
+
+
+
+
+link7 = "<a href='http://xlstat.com'>Data Analysis and Statistics with MS Excel, 30 Day Free Trial</a>"
+
+link3 = "<a href='http://statisticallysignificantconsulting.com/'><img src='stat_signficant.gif' width='177' height='84' border='0'></a><br /> Statistical Consulting Services for Doctoral Students and Researchers<br /><a href='http://statisticallysignificantconsulting.com/'>StatisticallySignificantConsulting.com</a>"
+
+link8 = "<a href='http://www.resample.com/'><img src='resampling.jpg' width='425' height='67' border='0'></a>"
+
+alinks= new Array(link2,link3,link4,link5,link6,link7,link8)
+/*
+st1 = "<a href = \"http://www.statisticssolutions.com/\" target=\"_top\">Dissertation and Thesis Methods and Statistical Consulting (877-437-8622)</a>"
+st2 = "<a href = \"http://www.statisticssolutions.com/\" target=\"_top\">Statistics and Methods Consulting For Graduate Students (877-437-8622)</a>"
+st3 = "<a href = \"http://www.statisticssolutions.com/\" target=\"_top\">Inexpensive Statistics and Method Section Consulting (877-437-8622)</a>"
+statSol = new Array(st1,st2,st3)
+*/
+
+function burst() {
+	tracking()
+}
+
+function top() {
+  if(parent.name != "HSframes") {
+	document.write("<p class='deep'><a href='index.html' target='_parent'>HyperStat Online Contents</a></p>")
+  }
+}
+
+function topofpage() {
+  if(parent.name != "HSframes") {
+	document.write("<p class='deep'><a href='index.html' target='_parent'>HyperStat Online Contents</a></p>")
+  }
+	tracking()
+}
+function tracking() {
+	
+
+document.write("<script src=\"http://www.google-analytics.com/urchin.js\" type=\"text/javascript\"></script><script type=\"text/javascript\">_uacct = \"UA-141388-1\";urchinTracker();</script>")
+
+
+}
+function home () {
+
+}
+
+function bottom1old() {
+ 
+document.write("<br clear = 'all'><br />")
+  
+}
+
+function bottom3() {
+
+
+}
+function bottomContents() {
+	
+}
+function bottom2() {
+ 	
+
+}
+
+
+
+function bottom1() {
+ 	
+       
+	var r = Math.random()
+var rs =Math.floor(Math.random()*3)
+//alert(rs)
+
+	
+mcgLink = "<a href='http://precisionconsultingcompany.com/dissertation-help.shtml' target='_top'>Statistical Consulting for Dissertations and Theses (302-407-0449)</a>"
+
+	
+<!--  document.write(mcgLink + "<br><br> <a href='http://www.directtextbook.com'>College Textbooks</a><br /><a href='http://www.directtextbook.com/books/mathematics-textbooks'>Math Textbooks</a><br><a href='http://www.directtextbook.com/books/statistics-textbooks'>Statistics Textbooks</a>")
+  
+-->
+document.write("<br>"+mcgLink+ "<br><br>")
+
+	
+	  document.write("<div id=\"Layerg\" style=\"position:absolute; background-color: #FFFFFF; left:550px; top:22px; width:120px; height:600px; z-index:1;\">")
+	 
+
+	document.write("<div align=\"center\">")
+	
+	google()
+	
+
+document.write("</div>")
+	
+		
+	
+
+  	
+}
+
+
+
+
+function googlea() {
+	//alert("a")
+	document.write("<br>")
+document.write("xscript type=\"text/javascript\"><!--")
+document.write("google_ad_client = \"pub-4811341167334986\";<br>")
+document.write("google_ad_width = 468;<br>")
+document.write("google_ad_height = 60;<br>")
+document.write("google_ad_format = \"468x60_as\";<br>")
+document.write("google_ad_type = \"text_image\";<br>")
+document.write("google_ad_channel = \"\";<br>")
+document.write("google_color_border = \"CC0000\";<br>")
+document.write("google_color_bg = \"FFFFCC\";<br>")
+document.write("google_color_link = \"0000FF\";<br>")
+document.write("google_color_text = \"000000\";<br>")
+document.write("google_color_url = \"008000\";<br>")
+document.write("//-->/xscript>")
+document.write("xscript type=\"text/javascript\"")
+ document.write(" src=\"http://pagead2.googlesyndication.com/pagead/show_ads.js\">")
+document.write("/xscript>")	
+}
+function google() {
+	
+
+document.write("<script language=\"JavaScript\">")
+document.write("google_ad_client = \'pub-4811341167334986\';")
+document.write("google_ad_width = 120;")
+document.write("google_ad_height = 600;")
+document.write("google_ad_format = \'120x600_as\';")
+
+
+document.write("google_page_url = document.location;")
+document.write("</script>")
+
+document.write("<script language=\"JavaScript\"")
+document.write(" src=\"http://pagead2.googlesyndication.com/pagead/show_ads.js\">;")
+document.write("</script>")
+}
+
+function email(){
+document.write("<a href=\"mailto:dmlane" + "@davidmlane.com\">dmlane@davidmlane.com</a>")	
+	
+}
